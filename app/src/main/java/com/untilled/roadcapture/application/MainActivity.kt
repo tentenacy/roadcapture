38b00f622cf400706ae09aef4b5c5923abb39f02
Wonseok Lee <tentenacy@gmail.com>
2021-08-06 17:12:01 +0900

    fix: device size is incompatible when status bar is transparent

--- app/src/main/java/com/untilled/roadcapture/application/MainActivity.kt
+++ app/src/main/java/com/untilled/roadcapture/application/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.untilled.roadcapture.application
 
-import android.content.Context
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.untilled.roadcapture.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -15,26 +14,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        makeStatusBarTransparent()
-    }
-
-    private fun makeStatusBarTransparent() {
-        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
-        binding.container.setPadding(0, getStatusBarHeight(this), 0, getNavigationBarHeight(this))
-    }
-
-    private fun getStatusBarHeight(context: Context): Int {
-        val resourceId = context.resources.getIdentifier("status_bar_height", "dimen", "android")
-
-        return if (resourceId > 0) context.resources.getDimensionPixelSize(resourceId)
-        else 0
-    }
-
-    private fun getNavigationBarHeight(context: Context): Int {
-        val resourceId = context.resources.getIdentifier("navigation_bar_height", "dimen", "android")
-
-        return if (resourceId > 0) context.resources.getDimensionPixelSize(resourceId)
-        else 0
     }
 }
