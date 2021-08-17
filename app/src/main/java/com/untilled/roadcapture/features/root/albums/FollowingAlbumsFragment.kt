80ff97e648c288a989b58d8260610298ad8dc07b
HANSANGWOO97 <hsw0715@naver.com>
2021-08-18 00:32:46 +0900

    design: fragment_following_albums

--- app/src/main/java/com/untilled/roadcapture/features/root/albums/FollowingAlbumsFragment.kt
+++ app/src/main/java/com/untilled/roadcapture/features/root/albums/FollowingAlbumsFragment.kt
@@ -1,11 +1,15 @@
 package com.untilled.roadcapture.features.root.albums
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.orhanobut.logger.Logger
+import com.untilled.roadcapture.application.MainActivity
 import com.untilled.roadcapture.databinding.FragmentFollowingAlbumsBinding
+import com.untilled.roadcapture.utils.DummyDataSet
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,6 +18,8 @@ class FollowingAlbumsFragment : Fragment() {
     private var _binding: FragmentFollowingAlbumsBinding? = null
     private val binding get() = _binding!!
 
+    private var isClicked = false
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,7 +27,31 @@ class FollowingAlbumsFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentFollowingAlbumsBinding.inflate(layoutInflater, container, false)
 
+        (requireActivity() as MainActivity).setSupportActionBar(binding.toolbarFollowingAlbums)
+
+        binding.recyclerviewFollowingAlbums.adapter = AlbumsAdapter(DummyDataSet.albums)
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+        binding.fbuttonFollowingAlbumsSort.setOnClickListener {
+            isClicked = if(isClicked){  // fbutton이 눌렸다면
+                ObjectAnimator.ofFloat(binding.fbuttonFollowingAlbumsSortPopularity,"translationY",0f).apply { start() }
+                ObjectAnimator.ofFloat(binding.fbuttonFollowingAlbumsSortLatest,"translationY",0f).apply { start() }
+                false
+            } else {  // fbutton이 눌리지 않았다면
+                ObjectAnimator.ofFloat(binding.fbuttonFollowingAlbumsSortPopularity,"translationY",-450f).apply { start() }
+                ObjectAnimator.ofFloat(binding.fbuttonFollowingAlbumsSortLatest,"translationY",-250f).apply { start() }
+                true
+            }
+        }
     }
 
     override fun onDestroyView() {
