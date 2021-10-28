30c5620c374360cf034e33cad6bba845daad904b
SangWooHan <hsw0715@naver.com>
2021-10-28 18:17:13 +0900

    feat: implement request camera permission using dexter library

--- app/src/main/java/com/untilled/roadcapture/features/root/capture/CaptureFragment.kt
+++ app/src/main/java/com/untilled/roadcapture/features/root/capture/CaptureFragment.kt
@@ -1,11 +1,14 @@
 package com.untilled.roadcapture.features.root.capture
 
+import android.Manifest
 import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.*
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
@@ -20,6 +23,15 @@ import com.untilled.roadcapture.data.entity.Picture
 import com.untilled.roadcapture.databinding.FragmentCaptureBinding
 import dagger.hilt.android.AndroidEntryPoint
 import androidx.core.net.toUri
+import com.google.android.material.snackbar.Snackbar
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.CompositePermissionListener
+import com.karumi.dexter.listener.single.PermissionListener
+import com.karumi.dexter.listener.single.SnackbarOnDeniedPermissionListener
 import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.overlay.OverlayImage
 import com.untilled.roadcapture.utils.getCircularBitmap
@@ -79,8 +91,7 @@ class CaptureFragment : Fragment(), OnMapReadyCallback {
 
     private fun setOnClickListeners() {
         binding.fabCaptureCapture.setOnClickListener {
-            Navigation.findNavController(binding.root)
-                .navigate(R.id.action_captureFragment_to_cameraFragment)
+            requestCameraPermission()
         }
         binding.imageviewCaptureBack.setOnClickListener {
             requireActivity().onBackPressed()
@@ -137,5 +148,34 @@ class CaptureFragment : Fragment(), OnMapReadyCallback {
         naverMap = _naverMap
 
         getNavArgs()
+    }
+
+    private fun requestCameraPermission() {
+        val basicPermissionListener : PermissionListener = object : PermissionListener {
+            override fun onPermissionGranted(p0: PermissionGrantedResponse?) {  // 권한 허용 됬을때
+                Navigation.findNavController(binding.root)
+                    .navigate(R.id.action_captureFragment_to_cameraFragment)
+            }
+            override fun onPermissionDenied(p0: PermissionDeniedResponse?) {    // 권한 거부 됬을대
+                Toast.makeText(requireContext(), "카메라 권한 요청이 거부되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+            override fun onPermissionRationaleShouldBeShown(p0: PermissionRequest?, p1: PermissionToken?) {
+            }
+        }
+
+        val snackbarPermissionListener: PermissionListener =
+            SnackbarOnDeniedPermissionListener.Builder
+                .with(view, "사진을 찍기위해서는 카메라 권한이 필요합니다. 설정으로 이동합니다.")
+                .withOpenSettingsButton("설정")
+                .withCallback(object : Snackbar.Callback() {
+                    override fun onShown(snackbar: Snackbar) {
+                    }
+                    override fun onDismissed(snackbar: Snackbar, event: Int) {
+                    }
+                }).build()
+
+        Dexter.withContext(requireContext())
+            .withPermission(Manifest.permission.CAMERA)
+            .withListener(CompositePermissionListener(basicPermissionListener, snackbarPermissionListener)).check()
     }
 }
