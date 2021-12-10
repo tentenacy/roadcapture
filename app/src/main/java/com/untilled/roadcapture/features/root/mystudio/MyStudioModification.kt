96a74cd920ef2249ca90817315b2a554c7178d5f
Kwang Hyun <kwangddang12@naver.com>
2021-12-10 19:18:53 +0900

    design: redesign fragment_my_studio

--- app/src/main/java/com/untilled/roadcapture/features/root/mystudio/MyStudioModification.kt
+++ app/src/main/java/com/untilled/roadcapture/features/root/mystudio/MyStudioModification.kt
@@ -11,6 +11,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
@@ -19,6 +20,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.untilled.roadcapture.R
+import com.untilled.roadcapture.application.MainActivity
 import com.untilled.roadcapture.core.navigation.StackHostFragment
 import com.untilled.roadcapture.data.entity.User
 import com.untilled.roadcapture.databinding.FragmentMyStudioModificationBinding
@@ -72,15 +74,14 @@ class MyStudioModification : Fragment() {
     ): View? {
         _binding = FragmentMyStudioModificationBinding.inflate(inflater,container,false)
         binding.user = args.user
-
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setOnClickListeners()
-        setEditTextChangeListeners()
+//        setOnClickListeners()
+//        setEditTextChangeListeners()
     }
 
     override fun onDestroyView() {
@@ -88,37 +89,37 @@ class MyStudioModification : Fragment() {
         _binding = null
     }
 
-    private fun setOnClickListeners(){
-        binding.imageviewMyStudioModificationCheck.setOnClickListener {
-            saveUserInfo()
-            Navigation.findNavController(binding.root).popBackStack()
-        }
-        binding.imageviewMyStudioModificationBack.setOnClickListener {
-            Navigation.findNavController(binding.root).popBackStack()
-        }
-        binding.textviewMyStudioModificationChangeProfileImage.setOnClickListener {
-            pickFromGallery(PROFILE)
-        }
-        binding.textviewMyStudioModificationChangeBackgroundImage.setOnClickListener {
-            pickFromGallery(BACKGROUND)
-        }
-    }
-
-    private fun setEditTextChangeListeners(){
-        binding.edittextMyStudioModificationChangeProfileDescription.setText(args.user?.description)
-        binding.edittextMyStudioModificationChangeProfileDescription.addTextChangedListener(object: TextWatcher{
-            override fun beforeTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                binding.textviewMyStudioModificationProfileDescription.text = text.toString()
-            }
-
-            override fun afterTextChanged(text: Editable?) {
-            }
-
-        })
-    }
+//    private fun setOnClickListeners(){
+//        binding.imageviewMyStudioModificationCheck.setOnClickListener {
+//            saveUserInfo()
+//            Navigation.findNavController(binding.root).popBackStack()
+//        }
+//        binding.imageviewMyStudioModificationBack.setOnClickListener {
+//            Navigation.findNavController(binding.root).popBackStack()
+//        }
+//        binding.textviewMyStudioModificationChangeProfileImage.setOnClickListener {
+//            pickFromGallery(PROFILE)
+//        }
+//        binding.textviewMyStudioModificationChangeBackgroundImage.setOnClickListener {
+//            pickFromGallery(BACKGROUND)
+//        }
+//    }
+//
+//    private fun setEditTextChangeListeners(){
+//        binding.edittextMyStudioModificationChangeProfileDescription.setText(args.user?.description)
+//        binding.edittextMyStudioModificationChangeProfileDescription.addTextChangedListener(object: TextWatcher{
+//            override fun beforeTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//            }
+//
+//            override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//                binding.textviewMyStudioModificationProfileDescription.text = text.toString()
+//            }
+//
+//            override fun afterTextChanged(text: Editable?) {
+//            }
+//
+//        })
+//    }
 
     private fun pickFromGallery(type: Int) {
         val intent = Intent(Intent.ACTION_PICK)
