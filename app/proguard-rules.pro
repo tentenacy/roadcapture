de40b3f71de9f8f4380c4bf128b0458a214d5b57
tenutz <tentenacy@gmail.com>
2022-01-14 16:23:53 +0900

    build: set gradle version to 7.0.0

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -22,3 +22,5 @@
 -keep public class com.nhn.android.naverlogin.** {
     public protected *;
 }
+-keep class com.kakao.sdk.**.model.* { <fields>; }
+-keep class * extends com.google.gson.TypeAdapter
--- app/src/main/java/com/untilled/roadcapture/application/GlobalApplication.kt
+++ app/src/main/java/com/untilled/roadcapture/application/GlobalApplication.kt
@@ -2,8 +2,10 @@ package com.untilled.roadcapture.application
 
 import android.app.Application
 import com.chibatching.kotpref.Kotpref
+import com.kakao.sdk.common.KakaoSdk
 import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.Logger
+import com.untilled.roadcapture.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 
 
@@ -13,5 +15,6 @@ class GlobalApplication: Application() {
         super.onCreate()
         Logger.addLogAdapter(AndroidLogAdapter())
         Kotpref.init(this)
+        KakaoSdk.init(this, BuildConfig.SOCIAL_NAVER_CLIENT_ID)
     }
 }
